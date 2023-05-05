35e05f0dbd34b648fe4d7b020d3b3bd5f314e497
Guilherme O. Souza <ShadowDragonRub@gmail.com>
2023-05-05 17:54:02 -0300

    V2

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 screen_width = 1280         #largura tela
 screen_height = 700         #altura tela
+vertical_tile_number = 100  #altura do mapa base do Tiled
+
+tiles_size = 32             #tamanho do bloco em pixels
 
 fps = 60                    #taxa máxima de quadros do jogo
