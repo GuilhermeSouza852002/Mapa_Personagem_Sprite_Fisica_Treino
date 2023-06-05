450e81c7a6937516c80580ebbbca99d7160beffc
Guilherme O. Souza <ShadowDragonRub@gmail.com>
2023-06-05 01:30:14 -0300

    Corrigir_mapa_player

--- level.py
+++ level.py
@@ -6,105 +6,95 @@ from player import *
 from game_data import directory1
 
 class Level:
-    def __init__(self,level_data,surface):  #O parâmetro level_data é um dicionário que provavelmente contém informações sobre o nível do jogo, como o mapa, os personagens, inimigos, itens, etc. O parâmetro surface é a superfície do Pygame na qual o nível será desenhado.
+    def __init__(self, level_data, surface):  #O parâmetro level_data é um dicionário que provavelmente contém informações sobre o nível do jogo, como o mapa, os personagens, inimigos, itens, etc. O parâmetro surface é a superfície do Pygame na qual o nível será desenhado.
         self.display_surface = surface  #inicializa a variável display_surface com a superfície recebida como parâmetro.
         
+        # sprite group setup
+        self.visible_sprites = CameraGroup()
+        self.active_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
+        
         player_layout = import_csv_layout(level_data['Player'])
-        self.player = pygame.sprite.GroupSingle()
-        #self.camera_group = CameraGroup()
         self.player_setup(player_layout)
         
         terrain_layout = import_csv_layout(level_data['Terrain'])
-        self.terrain_sprites = self.create_tile_group(terrain_layout, 'Terrain')    #chama o método create_tile_group para criar um grupo de sprites (imagens ou objetos que podem ser desenhados na tela) a partir do layout do terreno. O método create_tile_group recebe o layout do terreno e uma string 'Terrain' como parâmetros, e provavelmente retorna um grupo de sprites que representam o terreno do nível. Esses sprites podem ser desenhados na tela posteriormente usando a superfície display_surface.
+        self.terrain_sprites = self.create_tile_group(terrain_layout, 'Terrain')
         
-        #print dos tijolos do mapa
-        terrain_tiles_images = import_folder_images_dict(directory1) #função Python que usa a biblioteca Pygame para carregar imagens de uma pasta especificada e retorna um dicionário que mapeia os nomes das imagens (sem a extensão do arquivo) para objetos de imagem 
+        terrain_tiles_images = import_folder_images_dict(directory1)
         print(terrain_tiles_images)
         
-        #print do recorte da spritesheet
-        path = 'Graphics/Character/AnimationSheet_Character.png' 
-
+        path = 'Graphics/Character/AnimationSheet_Character.png'
         frames = import_folder(path, frame_width, frame_height)
-
-        # Verificar o conteúdo dos frames
+        
         for i, frames in enumerate(frames):
             print(f"Frames {i+1}:")
             print(frames)
             print("---")
         
-    #percorre as linhas do mapa e as númera
-    def create_tile_group(self,layout,type):
-        sprite_group = pygame.sprite.Group()    #criar um objeto pygame.sprite.Group(), que é um grupo de sprites vazio.
-        for row_index, row in enumerate(layout):    #lendo linha por linha do level_map horizontal
-            for col_index, val in enumerate(row):   #lendo colunas na vertical
-                if val != '-1': 
-                    #o método calcula as coordenadas x e y do sprite com base na linha e coluna atuais do loop, multiplicando-as pelo tamanho dos tiles do jogo (tiles_size).
-                    x = col_index * tiles_size  #obtendo a posição de x
-                    y = row_index * tiles_size  #obtendo a posição de y
+    def create_tile_group(self, layout, type):
+        sprite_group = pygame.sprite.Group()
+        for row_index, row in enumerate(layout):
+            for col_index, val in enumerate(row):
+                if val != '-1':
+                    x = col_index * tiles_size
+                    y = row_index * tiles_size
                    
                     if type == 'Terrain':
-                                sprite = Tile(tiles_size,x,y)
-                                
-                                terrain_tile_list = import_cut_graphics(directory1) #criando lista de sprites 
-                                #print(len(terrain_tile_list))
-                                
-                                tile_surface = terrain_tile_list[int(val)]  
-                                sprite = StaticTile(tiles_size,x,y,tile_surface) #criando sprite 
-                                sprite_group.add(sprite)    #adiciona as sprites ao sprite_group
+                        sprite = Tile(tiles_size, x, y)
+                        
+                        terrain_tile_list = import_cut_graphics(directory1)
+                        tile_surface = terrain_tile_list[int(val)]
+                        sprite = StaticTile(tiles_size, x, y, tile_surface)
+                        sprite_group.add(sprite)
                                 
         return sprite_group
         
-    def player_setup(self,layout):
+    def player_setup(self, layout):
         for row_index, row in enumerate(layout):
-            for col_index,val in enumerate(row):
+            for col_index, val in enumerate(row):
                 x = col_index * tiles_size
                 y = row_index * tiles_size
                 if val == '0':
-                    #print('Jogador aqui!')
-                    sprite = Player((x,y),self.display_surface)
-                    self.player.add(sprite)
+                    sprite = Player((x, y), [self.visible_sprites, self.active_sprites], self.collision_sprites)
+                    self.active_sprites.add(sprite)
                     
     def run(self):
+        
         self.terrain_sprites.draw(self.display_surface)
         self.terrain_sprites.update(0)
         
-        self.player.update()
-        self.player.draw(self.display_surface)  # chame o método custom_draw do objeto CameraGroup
+        self.active_sprites.update()
+        self.visible_sprites.custom_draw(self.active_sprites)
                     
-class CameraGroup(pygame.sprite.GroupSingle):	#herdando sprite.Group
-	def __init__(self):
-		super().__init__()
-		self.display_surface = pygame.display.get_surface()	#estamos dando ao CameraGroup o que queremos desenhar, assim não á necessidade de passar nenhum argumento para custom_draw
-		self.offset = pygame.math.Vector2(100,300) #posição superior a esquerda x e y
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2(100, 300)
 
-		# camera
-  		# devemos pegar a posição e o tamanho do retângulo
-		cam_left = camera_borders['left']	#cria uma borda do tamanho que foi definido o left
-		cam_top = camera_borders['top']		#cria uma borda do tamanho que foi definido o top
-		#descobrindo a altura e a lagurar do retângulo
-		cam_width = self.display_surface.get_size()[0] - (cam_left + camera_borders['right'])	#subtrai a largura total da tela left e right pra gerar a borda da tela da nova largura
-		cam_height = self.display_surface.get_size()[1] - (cam_top + camera_borders['bottom'])	#subtrai a autura total da tela top e bottom pra gerar a borda da tela da nova autura
+        cam_left = camera_borders['left']
+        cam_top = camera_borders['top']
+        cam_width = self.display_surface.get_size()[0] - (cam_left + camera_borders['right'])
+        cam_height = self.display_surface.get_size()[1] - (cam_top + camera_borders['bottom'])
 
-		self.camera_rect = pygame.Rect(cam_left,cam_top,cam_width,cam_height) #pegando a camera que sera movida pelo jogador
+        self.camera_rect = pygame.Rect(cam_left, cam_top, cam_width, cam_height)
 
-	def custom_draw(self,player):
-     	# movendo a camera
-		if player.rect.left < self.camera_rect.left:	#o player esta mais para a esquerda da camera
-			self.camera_rect.left = player.rect.left	#então a camera é destruida e jogada para a esquerda
-		if player.rect.right > self.camera_rect.right:	#o player esta mais para a direita da camera
-			self.camera_rect.right = player.rect.right	#então a camera é destruida e jogada para a direita
-		if player.rect.top < self.camera_rect.top:		#o player esta mais para cima da camera
-			self.camera_rect.top = player.rect.top		#então a camera é destruida e jogada para cima
-		if player.rect.bottom > self.camera_rect.bottom:#o player esta mais para baixo da camera
-			self.camera_rect.bottom = player.rect.bottom#então a camera é destruida e jogada para baixo
+    def custom_draw(self, player):
+        for sprite in player.sprites():  # Iterar sobre as sprites do player
+            if sprite.rect.left < self.camera_rect.left:
+                self.camera_rect.left = sprite.rect.left
+            if sprite.rect.right > self.camera_rect.right:
+                self.camera_rect.right = sprite.rect.right
+            if sprite.rect.top < self.camera_rect.top:
+                self.camera_rect.top = sprite.rect.top
+            if sprite.rect.bottom > self.camera_rect.bottom:
+                self.camera_rect.bottom = sprite.rect.bottom
 
-		# camera offset 
-		# deslocamento da camera
-		self.offset = pygame.math.Vector2(
-      	# pegando a posição superior esquerda da tela para que o jogador sempre esteja no centro da tela	
-			self.camera_rect.left - camera_borders['left'],
-			self.camera_rect.top - camera_borders['top'])
+        self.offset = pygame.math.Vector2(
+            self.camera_rect.left - camera_borders['left'],
+            self.camera_rect.top - camera_borders['top']
+        )
 
-		for sprite in self.sprites():	#replicando todos os elementos na tela
-			offset_pos = sprite.rect.topleft - self.offset #posição de deslocamento automatico
-			self.display_surface.blit(sprite.image,offset_pos) #desenhado todos os elementos na tela
+        for sprite in player.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
--- player.py
+++ player.py
@@ -4,8 +4,8 @@ from settings import *
 
 #Corpo do player
 class Player(pygame.sprite.Sprite):
-    def __init__(self,pos, surface):
-        super().__init__()
+    def __init__(self,pos, groups, collision_sprites):
+        super().__init__(groups)
         self.image = pygame.Surface((32,32))    #tamanho player
         self.image.fill('#FFFFFF')  #cor player
         self.rect = self.image.get_rect(topleft = pos)
@@ -13,9 +13,10 @@ class Player(pygame.sprite.Sprite):
         #movimentação do player
         self.direction = pygame.math.Vector2(0,0)
         self.speed = 5                           #velocidade player
-        #self.gravity = 0.8                       #gravidade
+        self.gravity = 0.8                       #gravidade
         self.jump_speed = 16                     #velocidade do pulo
-        self.display_surface = surface
+        self.collision_sprites = collision_sprites
+        #self.display_surface = surface
         self.on_floor = False                    #para o jogador não pular infinitamente, ela começa falsa
         
     def get_input(self):
@@ -30,12 +31,39 @@ class Player(pygame.sprite.Sprite):
             
         if keys[pygame.K_SPACE] and self.on_floor:  #o player so pode pular se apertar espaço e se on_floor for verdadeiro
             self.direction.y = -self.jump_speed
+        
+        #verifica se teve colisão de sprites na horizontal        
+    def horizontal_movement_collision(self):
+        for sprite in self.collision_sprites.sprites():
+            if sprite.rect.colliderect(self.rect):
+                if self.direction.x < 0:    #se a direção for menor que zero o player esta indo para esquerda
+                    self.rect.left = sprite.rect.right  #então a colisão joga o player pra direita
+                elif self.direction.x > 0:  #se a direção for maior que zero o player esta indo para direita
+                    self.rect.right = sprite.rect.left  #então a colisão joga o player pra esquerda
+    
+    #verifica se teve colisão de sprites na vertical  
+    def vertical_movement_collidion(self):
+        for sprite in self.collision_sprites.sprites():
+            if sprite.rect.colliderect(self.rect):
+                if self.direction.y > 0:    #se a direção for maior que zero o player esta descendo
+                    self.rect.bottom = sprite.rect.top  #então a colisão joga o player pra cima
+                    self.direction.y = 0    #zeramos a gravidade para o player não atravessar o chão
+                    self.on_floor = True    #se o jogador tocar o chão ele pode pular novamente com essa condição
+                elif self.direction.y < 0:  #se a direção for menor que zero o player esta subindo
+                    self.rect.top = sprite.rect.bottom  #então a colisão joga o player pra baixo
+                    self.direction.y = 0    #zeramos a gravidade para o player não planar ao atingir o teto
+                
+            if self.on_floor and self.direction.y != 0: # se subirmos ou descermos, assim evita que o jogador possa dar um pulo no ar
+                self.on_floor =  False  # o on_floor se torna falso novamente      
             
-    #def apply_gravity(self):
-        #self.direction.y += self.gravity
-        #self.rect.y += self.direction.y
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
     
     def update(self):
         self.get_input()
         self.rect.x += self.direction.x * self.speed
-        #self.apply_gravity()
+        self.horizontal_movement_collision()
+        self.apply_gravity()
+        self.vertical_movement_collidion()
+        
