2140fc44099f941147e51ed7f90799dd1636cf27
Guilherme O. Souza <ShadowDragonRub@gmail.com>
2023-07-08 20:18:54 -0300

    OK

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.transform #biblioteca de espelhamento
 from support import *
 from settings import *
 
@@ -31,7 +32,9 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos, surface):
         super().__init__()
         self.image = pygame.Surface((32, 32))  # tamanho player
-        self.image.fill('#FFFFFF')  # cor player
+        #self.image.fill('#FFFFFF')  # cor player
+        self.animation_dict = {}  # Dicionário para armazenar as diferentes animações
+        self.load_animations()  # Carregar as animações do jogador
         self.rect = self.image.get_rect(topleft=pos)
         
         # movimentação do player
@@ -42,25 +45,97 @@ class Player(pygame.sprite.Sprite):
 
         self.display_surface = surface
         self.current_x = None
-
+        self.current_animation = None  # Adicione essa linha para definir o atributo current_animation como None
+        
         # player status
         self.on_ground = False
         self.on_ceiling = False
         self.on_left = False
         self.on_right = False
+        
+        
+    #sprites do player
+    def load_animations(self):
+        spritesheet = pygame.image.load('Graphics/Character/AnimationSheet_Character.png')
+        
+        # Definir as dimensões dos sprites individuais
+        sprite_width = 32
+        sprite_height = 32
 
+        # Definir as coordenadas de recorte dos sprites para cada animação
+        parado_coordinates = [
+            (0, 0, sprite_width, sprite_height),    # Coordenadas do primeiro sprite de "pular"
+            (32, 0, sprite_width, sprite_height),   # Coordenadas do segundo sprite de "pular"
+            # Adicione mais coordenadas de recorte para os outros sprites de "pular"
+        ]
+        
+        pular_coordinates = [
+            (0, 160, sprite_width, sprite_height),    # Coordenadas do primeiro sprite de "pular"
+            (32, 160, sprite_width, sprite_height),   # Coordenadas do segundo sprite de "pular"
+            (64, 160, sprite_width, sprite_height),
+            (96, 160, sprite_width, sprite_height),
+            (128, 160, sprite_width, sprite_height),
+            (160, 160, sprite_width, sprite_height),
+            (192, 160, sprite_width, sprite_height),
+            (224, 160, sprite_width, sprite_height),
+            # Adicione mais coordenadas de recorte para os outros sprites de "pular"
+        ]
+
+        andar_direita_coordinates = [
+            (0, 96, sprite_width, sprite_height),    # Coordenadas do primeiro sprite de "andar para direita"
+            (32, 96, sprite_width, sprite_height),   # Coordenadas do segundo sprite de "andar para direita"
+            (64, 96, sprite_width, sprite_height),
+            (96, 96, sprite_width, sprite_height),
+            (128, 96, sprite_width, sprite_height),
+            (160, 96, sprite_width, sprite_height),
+            (192, 96, sprite_width, sprite_height),
+            (224, 96, sprite_width, sprite_height),
+            # Adicione mais coordenadas de recorte para os outros sprites de "andar para direita"
+        ]
+
+        andar_esquerda_coordinates = [
+            (224, 96, sprite_width, sprite_height),    # Coordenadas do primeiro sprite de "andar para esquerda"
+            (192, 96, sprite_width, sprite_height),   # Coordenadas do segundo sprite de "andar para esquerda"
+            (160, 96, sprite_width, sprite_height),
+            (128, 96, sprite_width, sprite_height),
+            (96, 96, sprite_width, sprite_height),
+            (64, 96, sprite_width, sprite_height),
+            (32, 96, sprite_width, sprite_height),
+            (0, 96, sprite_width, sprite_height),
+            # Adicione mais coordenadas de recorte para os outros sprites de "andar para esquerda"
+        ]
+        # Carregar os sprites das animações e adicioná-los ao dicionário de animações
+        self.animation_dict['parado'] = self.load_sprites(spritesheet, parado_coordinates)
+        self.animation_dict['pular'] = self.load_sprites(spritesheet, pular_coordinates)
+        self.animation_dict['andar_direita'] = self.load_sprites(spritesheet, andar_direita_coordinates)
+        self.animation_dict['andar_esquerda'] = self.load_sprites(spritesheet, andar_esquerda_coordinates)
+        
+    def load_sprites(self, spritesheet, coordinates):
+        sprites = []
+        for coords in coordinates:
+            sprite_image = spritesheet.subsurface(pygame.Rect(coords))
+            sprites.append(sprite_image)
+        return sprites
+        
     def get_input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:  # direita
             self.direction.x = 1
+            self.current_animation = 'andar_direita'  # Definir a animação atual
+            self.image = self.animation_dict[self.current_animation][0]  # Usar o primeiro frame da animação original
         elif keys[pygame.K_LEFT]:  # esquerda
             self.direction.x = -1
+            self.current_animation = 'andar_esquerda'  # Definir a animação atual
+            self.image = pygame.transform.flip(self.animation_dict[self.current_animation][0], True, False)  # Espelhar o primeiro frame da animação
         else:
             self.direction.x = 0
+            self.current_animation = 'parado'  # Definir a animação atual
 
-        if keys[pygame.K_SPACE] and self.on_ground:  # o player so pode pular se apertar espaço e se on_floor for verdadeiro
+        if keys[pygame.K_SPACE] and self.on_ground:  # o player só pode pular se apertar espaço e se on_floor for verdadeiro
             self.direction.y = -self.jump_speed
+            self.current_animation = 'pular'  # Definir a animação atual
+
 
     def horizontal_movement_collision(self, terrain_sprites):
         player = self
@@ -121,3 +196,8 @@ class Player(pygame.sprite.Sprite):
         self.get_player_on_ground()
         self.apply_gravity()
         self.vertical_movement_collision(terrain_sprites)
+        # Atualizar a imagem do sprite com base na animação atual
+        if self.current_animation is not None:
+            sprites = self.animation_dict[self.current_animation]
+            sprite_index = (pygame.time.get_ticks() // 100) % len(sprites)
+            self.image = sprites[sprite_index]
